1e587f00b2c1937e9a79595498901f9cefd8859f
ManlinLi <33075607+ManlinLi@users.noreply.github.com>
2018-04-22 22:36:59 -0400

    Q2-6

---
+++ src/main/java/net/home/Chapter2Question6.java
@@ -0,0 +1,12 @@
+package net.home;
+
+public class Chapter2Question6 {
+    public static void main (String[] args){
+        int ctr=50;
+        ctr+=1;    //or ++ctr
+        System.out.println(ctr % 20);
+
+
+        //output 11
+    }
+}
