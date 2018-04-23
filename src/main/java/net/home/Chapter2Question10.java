6737175cfa2315baba0e70f07d55547c5111c48e
ManlinLi <33075607+ManlinLi@users.noreply.github.com>
2018-04-22 22:49:13 -0400

    Q2-10

---
+++ src/main/java/net/home/Chapter2Question10.java
@@ -0,0 +1,14 @@
+package net.home;
+
+public class Chapter2Question10 {
+    public static void main (String[] args){
+        boolean myBool=false;
+        int yourInt = 10;
+        float hisFloat = 19.54f;
+        System.out.println(hisFloat=yourInt); //10.0    using = not ==
+        System.out.println(yourInt>10);  //false
+        System.out.println(myBool=false);  //false
+
+
+    }
+}
