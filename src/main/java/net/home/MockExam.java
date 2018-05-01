39e8b4def50c519b7bb5e3bc75e47f23f7884b6d
ManlinLi <33075607+ManlinLi@users.noreply.github.com>
2018-05-01 18:03:43 -0400

    q2

--- src/main/java/net/home/MockExam.java
+++ src/main/java/net/home/MockExam.java
@@ -2,10 +2,13 @@ package net.home;
 
 public class MockExam {
     public static void main(String[] args) {
-        int []a = {1,2,3,4,5,6};
-        int i = a.length - 1;
-        while(i>=0){
-        System.out.print(a[i]);i--;
-        }
+        int x = 0, y=10;
+       try{
+           y /=x;
+       }
+       System.out.print("/ by 0");
+       catch(Exception e){  //error
+         System.out.print("error");
+       }
     }
 }
