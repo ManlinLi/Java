893644a5c1ad60818b131b8f60257f072686393d
ManlinLi <33075607+ManlinLi@users.noreply.github.com>
2018-05-01 18:17:09 -0400

    q12

---
+++ src/main/java/net/home/Q12.java
@@ -0,0 +1,14 @@
+package net.home;
+
+public class Q12 {
+
+  Q12(String s,int i ){
+      ++pid;
+      name=s;
+      age=i;
+      }
+      static int pid;
+      int age;
+      String name;
+  }
+
