c4aad72b0d8b45e7ffc3fc2b30d3c5e08b5599ee
ManlinLi <33075607+ManlinLi@users.noreply.github.com>
2018-05-01 18:20:26 -0400

    q14

--- src/main/java/net/home/MockExam.java
+++ src/main/java/net/home/MockExam.java
@@ -3,13 +3,10 @@ package net.home;
 public class MockExam {
     static int x = 10;
     public static void main(String[] args) {
-        int x = 10;
-        int y = new MockExam().change(x);
-        System.out.print(x+y);
-    }
-        int change(int x){
-        x=12;
-        return x;
+        String s = "Java";
+        s.concat(" SE 6");
+        s.replace('6','7');
+        System.out.print(s);
         }
     }
 
