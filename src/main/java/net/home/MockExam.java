cb93f7abba9c549369a3bf5382d2b5293bdfc022
ManlinLi <33075607+ManlinLi@users.noreply.github.com>
2018-05-01 18:19:17 -0400

    q13

--- src/main/java/net/home/MockExam.java
+++ src/main/java/net/home/MockExam.java
@@ -3,9 +3,13 @@ package net.home;
 public class MockExam {
     static int x = 10;
     public static void main(String[] args) {
-        for(int x=0;x<5;x++){
-         }
-        System.out.print(x);
+        int x = 10;
+        int y = new MockExam().change(x);
+        System.out.print(x+y);
+    }
+        int change(int x){
+        x=12;
+        return x;
         }
     }
 
