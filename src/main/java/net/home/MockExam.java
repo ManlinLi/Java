33729ed5dbd824d438fd573cc63229d76e82d08d
ManlinLi <33075607+ManlinLi@users.noreply.github.com>
2018-05-01 18:08:18 -0400

    q7

--- src/main/java/net/home/MockExam.java
+++ src/main/java/net/home/MockExam.java
@@ -2,10 +2,10 @@ package net.home;
 
 public class MockExam {
     public static void main(String[] args) {
-        int a[] = { 1,2,053,4};
-        int b[][] = { {1,2,4} , {2,2,1},{0,43,2}};
-        System.out.print(a[3]==b[0][2] );
-        System.out.print(" " + (a[2]==b[2][1]));
+        int a[][] = new int[3][];
+        a[1] = new int[]{1,2,3};
+        a[2] = new int[]{4,5};
+        System.out.print(a[1][1]);
        }
     }
 
